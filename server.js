9abdb7c12abc9bf598035deacdd02a7c503abfd8
Aruneswaran-31 <aruneswaran25@gmail.com>
2025-12-29 12:31:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.get("/", (req, res) => {
   res.send("Backend is LIVE on Railway");
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port", PORT);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 // Keep alive (Railway-safe)
