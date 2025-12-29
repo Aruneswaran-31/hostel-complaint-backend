8389dda13dfc2bd8b637339ec829b3b1e688f474
Aruneswaran-31 <aruneswaran25@gmail.com>
2025-12-29 11:17:23 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,68 +1,25 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const db = require("../db");
-
 const router = express.Router();
 
-if (process.env.RENDER) {
-  module.exports = require("express").Router();
-  return;
-}
-
-/* REGISTER */
-router.post("/register", async (req, res) => {
-  const { name, email, password, role } = req.body;
-
-  if (!name || !email || !password || !role) {
-    return res.status(400).json({ message: "All fields required" });
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const sql =
-    "INSERT INTO users (name, email, password, role) VALUES (?, ?, ?, ?)";
-
-  db.query(sql, [name, email, hashedPassword, role], (err) => {
-    if (err) {
-      return res.status(500).json({ message: "User already exists" });
-    }
-    res.json({ message: "User registered successfully" });
-  });
-});
-
-/* LOGIN */
+// MOCK LOGIN
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
 
-  db.query(
-    "SELECT * FROM users WHERE email = ?",
-    [email],
-    async (err, result) => {
-      if (result.length === 0) {
-        return res.status(400).json({ message: "User not found" });
-      }
+  if (email === "student@test.com" && password === "123456") {
+    return res.json({
+      token: "mock-student-token",
+      role: "student",
+    });
+  }
 
-      const user = result[0];
-      const isMatch = await bcrypt.compare(password, user.password);
+  if (email === "admin@test.com" && password === "123456") {
+    return res.json({
+      token: "mock-admin-token",
+      role: "admin",
+    });
+  }
 
-      if (!isMatch) {
-        return res.status(400).json({ message: "Invalid password" });
-      }
-
-      const token = jwt.sign(
-        { id: user.id, role: user.role },
-        "SECRET_KEY",
-        { expiresIn: "1d" }
-      );
-
-      res.json({
-        token,
-        role: user.role,
-        userId: user.id
-      });
-    }
-  );
+  return res.status(401).json({ message: "Invalid credentials" });
 });
 
 module.exports = router;
